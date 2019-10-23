931ce8dcda1e7102f5a03384a62fc01bcfe4d209
mesielepush <48403308+mesielepush@users.noreply.github.com>
2019-10-23 18:27:23 -0500

    simple behaviour

--- zombie.rb
+++ zombie.rb
@@ -1,4 +1,26 @@
 require 'ruby2d'
 
 set background: 'green'
+
+message = Text.new('Click to begin')
+game_started = false
+square = nil
+
+on :mouse_down do |event|
+  puts event.x, event.y
+  if game_started
+    if square.contains?(event.x, event.y)
+      puts 'Click on square'
+    end
+  else
+    message.remove
+
+    square = Square.new(
+                        x: 100, y: 200,
+                        size: 125,
+                        color: 'purple'
+                      )
+    game_started = true
+  end
+end
 show
