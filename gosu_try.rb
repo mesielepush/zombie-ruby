f64f0c861b43fc6a53c1f50e4e6b2a12f5adf63e
Jonathan <48403308+mesielepush@users.noreply.github.com>
2019-10-27 16:00:52 -0600

    cover arrenging sprite ready

--- gosu_try.rb
+++ gosu_try.rb
@@ -1,97 +1,95 @@
 require 'gosu'
+################################### CLASSES ################################
+class Animation
+  def initialize(frames, time_in_secs)
+    @frames = frames
+    @time = time_in_secs * 1000    
+  end
 
-def media_path(file)
-  File.join(File.dirname(File.dirname(
-  __FILE__)), 'media', file)
+  def start
+    @frames[Gosu::milliseconds / @time % @frames.size]
+  end
+
+  def stop
+    @frames[0]
+  end
 end
 
-class Explosion
-  FRAME_DELAY = 10 # ms
-  SPRITE = 'coin.png'
-
-  def self.load_animation(window)
-    Gosu::Image.load_tiles(
-    window, SPRITE, 35, 300, true)
-  end
-
-  def initialize(animation, x, y)
-    @animation = animation
+class Eye_candy
+  def initialize(x, y)
+    @frames = Gosu::Image.load_tiles 'mario2.png', 46, 143
     @x, @y = x, y
-    @current_frame = 0
-  end
+    
+    @move = {:left => Animation.new(@frames, 0.05),
+             :right => Animation.new(@frames, 0.05)}
 
-  def update
-    @current_frame += 1 if frame_expired?
+    @movements = {:left => -2.0, :right => 2.0}
+
+    @moving = false
+    @facing = :left
   end
 
   def draw
-    return if done?
-    image = current_frame
-    image.draw(
-    @x - image.width / 2.0,
-    @y - image.height / 2.0,
-    0)
-  end
-
-  def done?
-    @done ||= @current_frame == @animation.size
-  end
-
-  private
-
-  def current_frame
-    @animation[@current_frame % @animation.size]
-  end
-
-  def frame_expired?
-    now = Gosu.milliseconds
-    @last_frame ||= now
-    if (now - @last_frame) > FRAME_DELAY
-    @last_frame = now
+    if @moving
+      @move[@facing].start.draw @x, @y, 1
+    else
+      @move[@facing].stop.draw @x, @y, 1
     end
   end
+
+  def move(direction)
+    @x += @movements[direction]
+    @x %= 640
+    
+    @facing = direction
+    @moving = true if @moving != true
+  end
+
+  def stop_move
+    @moving = false if @moving != false
+  end
 end
+
+################################### GAME MAIN ################################
 
 class GameWindow < Gosu::Window
-  BACKGROUND = 'back.png'
+  def initialize
+    super 700, 700
+    @player = Eye_candy.new 0, 440
 
-  def initialize(width=700, height=700, fullscreen=false)
-    super
-    self.caption = 'Hello Animation'
-    @background = Gosu::Image.new(
-    self, BACKGROUND,z:-1)
-    @animation = Explosion.load_animation(self)
-    @explosions = []
+    @key = {kb_left: Gosu::KbLeft,
+            kb_right: Gosu::KbRight,
+            gp_left: Gosu::GpLeft,
+            gp_right: Gosu::GpRight,
+            enter: Gosu::KB_RETURN
+            }
+    @background = Gosu::Image.new("back.png")
   end
 
   def update
-    @explosions.reject!(&:done?)
-    @explosions.map(&:update)
-  end
-
-  def button_down(id)
-    close if id == Gosu::KbEscape
-    if id == Gosu::MsLeft
-    @explosions.push(
-    Explosion.new(
-    @animation, mouse_x, mouse_y))
+    if Gosu::button_down? @key[:kb_left] or Gosu::button_down? @key[:gp_left] then
+      @player.move :left
     end
-  end
-
-  def needs_cursor?
-    true
-  end
-
-  def needs_redraw?
-    !@scene_ready || @explosions.any?
+    if Gosu::button_down? @key[:kb_right] or Gosu::button_down? @key[:gp_right] then
+      @player.move :right
+    end
+    puts Gosu::button_down? @key[:enter]
   end
 
   def draw
-    @scene_ready ||= true
     @background.draw(0, 0, 0)
-    @explosions.map(&:draw)
+    @player.draw
+
+  end
+
+  def button_up(id)
+    if id == @key[:kb_left] or id == @key[:gp_left] then
+      @player.stop_move
+    end
+    if id == @key[:kb_right] or id == @key[:gp_right] then
+      @player.stop_move
+    end
   end
 end
 
-window = GameWindow.new
-window.show
+GameWindow.new.show
