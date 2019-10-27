5a85c1c856c80c18443be52be1774384c492409b
Jonathan <48403308+mesielepush@users.noreply.github.com>
2019-10-27 13:14:49 -0600

    moving stuff

--- gosu_try.rb
+++ gosu_try.rb
@@ -1,17 +1,50 @@
 require 'gosu'
 
- class GameWindow < Gosu::Window
-   def initialize(width=320, height=240, fullscreen=false)
-     super
-       self.caption = 'Hello'
-        @message = Gosu::Image.from_text(
-                              self, 'Hello, World!', Gosu.default_font_name, 30)
-   end
+class GameWindow < Gosu::Window
+  def initialize(width=320, height=240, fullscreen=false)
+    super
+    self.caption = 'Hello Movement'
+    @x = @y = 10
+    @draws = 0
+    @buttons_down = 0
+    @font = Gosu::Font.new(self, Gosu::default_font_name, 20)
+  end
+
+  def update
+    @x -= 1 if button_down?(Gosu::KbLeft)
+    @x += 1 if button_down?(Gosu::KbRight)
+    @y -= 1 if button_down?(Gosu::KbUp)
+    @y += 1 if button_down?(Gosu::KbDown)
+  end
+
+  def button_down(id)
+    close if id == Gosu::KbEscape
+    @buttons_down += 1
+  end
+
+  def button_up(id)
+    @buttons_down -= 1
+  end
+
+  def needs_redraw?
+    @draws == 0 || @buttons_down > 0
+  end
 
   def draw
-    @message.draw(100, 10, 10)
-  end
- end
+    @draws += 1
+    
 
- window = GameWindow.new
- window.show
+    @font.draw_text(info, @x, @y, 1, 1.0, 1.0, Gosu::Color::WHITE)
+
+    
+  end
+
+  private
+
+  def info
+    "[x:#{@x};y:#{@y};draws:#{@draws}]"
+  end
+end
+
+window = GameWindow.new
+window.show
